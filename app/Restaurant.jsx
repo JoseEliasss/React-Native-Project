29def83a00d9062d8865f303f33bf3e1666aa039
Jose Elias <josejosephelias@gmail.com>
2024-12-05 14:48:32 +0200

    divided the restaurant into restaurantCard and restaurant

--- app/Restaurant.jsx
+++ app/Restaurant.jsx
@@ -1,32 +1,23 @@
+// Restaurants.js
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { View, Text, ScrollView, SafeAreaView, StyleSheet } from "react-native";
 import { FIREBASE_DB } from "../FirebaseCofing"; // Ensure correct Firebase import
 import { collection, getDocs, updateDoc, doc } from "firebase/firestore";
 import Hero from "./Hero";
 import HomeHero from "../assets/images/HomeHero.jpeg";
 import Status from "./Status";
-import ratingStar from "../assets/images/ratingStar.png";
+import RestaurantCard from "./RestaurantCard"; // Import the RestaurantCard component
 
 const Restaurants = () => {
   const [restaurants, setRestaurants] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Fetch restaurant data from Firestore
     const fetchRestaurants = async () => {
       try {
-        const restaurantsCollection = collection(FIREBASE_DB, "restaurant"); // Corrected collection name
+        const restaurantsCollection = collection(FIREBASE_DB, "restaurant");
         const snapshot = await getDocs(restaurantsCollection);
 
-        // Check if data is returned from Firestore
         if (snapshot.empty) {
           setLoading(false);
           return;
@@ -48,18 +39,16 @@ const Restaurants = () => {
     fetchRestaurants();
   }, []);
 
-  const handleFavoriteToggle = async (name, favorite) => {
+  const handleFavoriteToggle = async (id, favorite) => {
     try {
-      // Toggle the favorite state
-      const restaurantRef = doc(FIREBASE_DB, "restaurant", name);
+      const restaurantRef = doc(FIREBASE_DB, "restaurant", id);
       await updateDoc(restaurantRef, {
         favorite: !favorite,
       });
 
-      // Update the local state to reflect the change
       setRestaurants((prevRestaurants) =>
         prevRestaurants.map((restaurant) =>
-          restaurant.name === name
+          restaurant.id === id
             ? { ...restaurant, favorite: !favorite }
             : restaurant
         )
@@ -91,50 +80,17 @@ const Restaurants = () => {
 
             {/* Display the list of restaurants */}
             <View style={styles.restaurantPadding}>
-              <View style={styles.restaurantList}>
-                {restaurants.length > 0 ? (
-                  restaurants.map((restaurant, index) => (
-                    <View key={index} style={styles.restaurantItem}>
-                      <Image source={HomeHero} style={styles.image} />
-                      <View style={styles.name_location_rating}>
-                        <Text style={styles.restaurantName}>
-                          {restaurant.name} - {restaurant.location}
-                        </Text>
-
-                        <View style={styles.rating}>
-                          <Image
-                            source={ratingStar}
-                            style={styles.ratingStar}
-                          />
-                          <Text style={{ fontWeight: "bold" }}>
-                            {restaurant.rating}
-                          </Text>
-                        </View>
-                      </View>
-                      <Text style={styles.delivery}>
-                        Delivery: {restaurant.delivery}
-                      </Text>
-
-                      {/* Favorite Button (Positioned Top-Right) */}
-                      <TouchableOpacity
-                        style={styles.favoriteButton}
-                        onPress={() =>
-                          handleFavoriteToggle(
-                            restaurant.id,
-                            restaurant.favorite
-                          )
-                        }
-                      >
-                        <Text style={styles.favoriteIcon}>
-                          {restaurant.favorite ? "❤️" : "🤍"}
-                        </Text>
-                      </TouchableOpacity>
-                    </View>
-                  ))
-                ) : (
-                  <Text>No restaurants available.</Text>
-                )}
-              </View>
+              {restaurants.length > 0 ? (
+                restaurants.map((restaurant) => (
+                  <RestaurantCard
+                    key={restaurant.id}
+                    restaurant={restaurant}
+                    onFavoriteToggle={handleFavoriteToggle}
+                  />
+                ))
+              ) : (
+                <Text>No restaurants available.</Text>
+              )}
             </View>
           </View>
         </View>
@@ -173,62 +129,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
-  restaurantList: {
-    marginTop: 20,
-    width: "100%",
-  },
   restaurantPadding: {
     padding: 10,
-  },
-  restaurantItem: {
-    marginBottom: 20,
-    alignItems: "center",
-    borderWidth: 1,
-    borderRadius: 20,
-    borderColor: "#F8F8FF",
-    position: "relative", // Allow positioning of the favorite button inside the container
-  },
-  image: {
-    height: 200,
-    width: "100%",
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    marginBottom: 7,
-  },
-  name_location_rating: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "white",
-    width: "100%",
-  },
-  restaurantName: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  rating: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    width: 40,
-  },
-  ratingStar: {
-    width: 20,
-    height: 20,
-  },
-  delivery: {
-    width: "100%",
-    textAlign: "left",
-    marginTop: 7,
-  },
-  favoriteButton: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    padding: 5,
-  },
-  favoriteIcon: {
-    fontSize: 25,
   },
 });
 
---
+++ app/RestaurantCard.jsx
@@ -0,0 +1,91 @@
+// RestaurantCard.js
+import React from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import ratingStar from "../assets/images/ratingStar.png";
+
+const RestaurantCard = ({ restaurant, onFavoriteToggle }) => {
+  return (
+    <View style={styles.restaurantItem}>
+      <Image
+        source={require("../assets/images/HomeHero.jpeg")}
+        style={styles.image}
+      />
+      <View style={styles.name_location_rating}>
+        <Text style={styles.restaurantName}>
+          {restaurant.name} - {restaurant.location}
+        </Text>
+
+        <View style={styles.rating}>
+          <Image source={ratingStar} style={styles.ratingStar} />
+          <Text style={{ fontWeight: "bold" }}>{restaurant.rating}</Text>
+        </View>
+      </View>
+      <Text style={styles.delivery}>Delivery: {restaurant.delivery}</Text>
+
+      {/* Favorite Button (Positioned Top-Right) */}
+      <TouchableOpacity
+        style={styles.favoriteButton}
+        onPress={() => onFavoriteToggle(restaurant.id, restaurant.favorite)}
+      >
+        <Text style={styles.favoriteIcon}>
+          {restaurant.favorite ? "❤️" : "🤍"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  restaurantItem: {
+    marginBottom: 20,
+    alignItems: "center",
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: "#F8F8FF",
+    position: "relative", // Allow positioning of the favorite button inside the container
+  },
+  image: {
+    height: 200,
+    width: "100%",
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    marginBottom: 7,
+  },
+  name_location_rating: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    width: "100%",
+  },
+  restaurantName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 40,
+  },
+  ratingStar: {
+    width: 20,
+    height: 20,
+  },
+  delivery: {
+    width: "100%",
+    textAlign: "left",
+    marginTop: 7,
+  },
+  favoriteButton: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    padding: 5,
+  },
+  favoriteIcon: {
+    fontSize: 25,
+  },
+});
+
+export default RestaurantCard;
